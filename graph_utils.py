a4642848e1818c8f8b6314bd8e376922deebf4ae
Kawin Nikomborirak <concavemail@gmail.com>
2019-07-03 23:07:12 +0000

    Add ordered edge finder

--- graph_utils.py
+++ graph_utils.py
@@ -76,3 +76,62 @@ def connected_components(graph):
     return [Graph(vertices={k: graph.vertices[k] for k in group[0]},
                   edges={k: graph.edges[k] for k in group[1]})
             for group in groups]
+
+
+def ordered_odometry_edges(graph):
+    """Generate a list of a list of edges ordered by start of path to end.
+
+    The lists are different connected paths.
+    As long as the graph is connected, the output list should only one
+    list of edges.
+
+    Args:
+        graph: The graph to extract the ordered edges from.
+
+    Returns: A list of lists of edge UIDs, where each sublist is a
+        sequence of connected edges.
+    """
+    segments = []
+
+    for uid in graph.edges:
+        edge = graph.edges[uid]
+        if graph.vertices[edge.startuid].mode != VertexType.ODOMETRY \
+                or graph.vertices[edge.enduid].mode != VertexType.ODOMETRY:
+            continue
+        start_found = end_found = False
+        start_found_idx = end_found_idx = 0
+
+        for i in range(len(segments) - 1, -1, -1):
+            current_start_found = edge.startuid == graph.edges[segments[i][-1]].enduid
+            current_end_found = edge.enduid == graph.edges[segments[i][0]].startuid
+
+            if current_start_found:
+                start_found = True
+                start_found_idx = i
+
+            elif current_end_found:
+                end_found = True
+                end_found_idx = i
+
+            if current_start_found and end_found:
+                segments[i].append(uid)
+                segments[i].extend(segments[end_found_idx])
+                del segments[end_found_idx]
+                break
+
+            elif current_end_found and start_found:
+                segments[start_found_idx].append(uid)
+                segments[i] = segments[start_found_idx] + segments[i]
+                del segments[start_found_idx]
+                break
+
+        if start_found and not end_found:
+            segments[start_found_idx].append(uid)
+
+        elif end_found and not start_found:
+            segments[end_found_idx].insert(0, uid)
+
+        elif not (start_found or end_found):
+            segments.append([uid])
+
+    return segments
