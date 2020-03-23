c054009e4bf09dda9c9cb534267888f9de7c4e6d
Kawin Nikomborirak <concavemail@gmail.com>
2020-03-23 21:09:44 +0000

    Document json converter

--- convert_json.py
+++ convert_json.py
@@ -8,6 +8,14 @@ import graph_utils
 
 
 def pose2diffs(poses):
+    """Convert an array of poses in the odom frame to an array of
+    transformations from the last pose.
+
+    Args:
+      poses (np.ndarray): Pose or array of poses.
+    Returns:
+      An array of transformations
+    """
     diffs = []
     for previous_pose, current_pose in zip(poses[:-1], poses[1:]):
         diffs.append(np.linalg.inv(previous_pose).dot(current_pose))
@@ -16,12 +24,30 @@ def pose2diffs(poses):
 
 
 def matrix2measurement(pose):
+    """ Convert a pose or array of poses in matrix form to [x, y, z,
+    qx, qy, qz, qw].
+
+    The output will have one fewer dimension than the input.
+
+    Args:
+      pose (np.ndarray): Pose or array of poses in matrix form.
+        The poses are converted along the last two axes.
+    Returns:
+      Converted pose or array of poses.
+    """
     translation = pose[..., :3, 3]
     rotation = R.from_matrix(pose[..., :3, :3]).as_quat()
     return np.concatenate([translation, rotation], axis=-1)
 
 
 def as_graph(dct):
+    """Convert a dictionary decoded from JSON into a graph.
+
+    Args:
+      dct (dict): The dictionary to convert to a graph.
+    Returns:
+      A graph derived from the input dictionary.
+    """
     tag_data = np.array(dct['tag_data'])
     pose_data = np.array(dct['pose_data'])
     map_id = dct['map_id']
@@ -35,17 +61,22 @@ def as_graph(dct):
     tag_edge_measurements_matrix = tag_data[:, 1:17].reshape(-1, 4, 4)
     tag_edge_measurements = matrix2measurement(tag_edge_measurements_matrix)
 
+    # Enable lookup of tags by the frame they appear in
     tags_by_frame = {}
 
     for i, tag_frame in enumerate(tag_data[:, 18]):
         tags_by_frame[int(tag_frame)] = tags_by_frame.get(tag_frame, [])
         tags_by_frame[int(tag_frame)].append(i)
 
+    # Construct the dictionaries of vertices and edges
     vertices = {}
     edges = {}
     vertex_counter = 0
     edge_counter = 0
     counted_tags_vertex_ids_by_tag_id = {}
+
+    previous_vertex = None
+    previous_pose_matrix = None
 
     for i, odom_frame in enumerate(pose_data[:, 17]):
         odom_vertex = vertex_counter
@@ -57,8 +88,8 @@ def as_graph(dct):
 
         vertex_counter += 1
 
+        # Connect odom to tag vertex
         for tag_idx in tags_by_frame.get(int(odom_frame), []):
-            # if tag_data[tag_idx, 0] not in counted_tags:
             if not counted_tags_vertex_ids_by_tag_id.get(tag_data[tag_idx, 0]):
                 vertices[vertex_counter] = graph.Vertex(
                     mode=graph.VertexType.TAG,
@@ -84,10 +115,23 @@ def as_graph(dct):
             vertex_counter += 1
             edge_counter += 1
 
+        if previous_vertex:
+            edges[edge_counter] = graph.Edge(
+                startuid=previous_vertex,
+                enduid=odom_vertex,
+                information=np.eye(6),
+                measurement=matrix2measurement(np.linalg.inv(
+                    previous_pose_matrix).dot(pose_matrices[i]))
+            )
+            edge_counter += 1
+        previous_vertex = odom_vertex
+        previous_pose_matrix = pose_matrices[i]
+
     resulting_graph = graph.Graph(vertices, edges)
     return resulting_graph
 
 
+# Testing code
 with open('round1.json', 'r') as f:
     x = json.load(f)
 
