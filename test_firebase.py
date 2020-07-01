3886bde93cd494b672a5f2ba0a9b5f85561803a7
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-07-01 15:10:08 -0400

    add continuous processing option

--- test_firebase.py
+++ test_firebase.py
@@ -13,18 +13,24 @@ from firebase_admin import storage
 import os
 
 
-def process_map(x):
+def optimize_map(x, tune_weights=False, visualize=False):
     test_graph = convert_json.as_graph(x)
 
     test_graph.weights = np.array([
-        0.,  0.,  0., 0.,  0.,  0.,
-        0.,  0.,  0., 0.,  0.,  0.,
-        0.,  0.,  0., -1e1,  1e2,  1e2
+        3.,  3.,  3., 3.,  3.,  3.,
+        1.6,  1.6,  1.6, 1.6,  1.6,  1.6,
+        0.,  0.,  0., -1e1,  3,  3
     ])
 
     # Load these weights into the graph
     test_graph.update_edges()
-
+    test_graph.generate_unoptimized_graph()
+    starting_map = graph_utils.optimizer_to_map(
+        test_graph.vertices, test_graph.unoptimized_graph)
+    original_tag_verts = starting_map['tags']
+    if tune_weights:
+        test_graph.expectation_maximization_once()
+        print("tuned weights", test_graph.weights)
     # Create the g2o object and optimize
     test_graph.generate_unoptimized_graph()
     test_graph.optimize_graph()
@@ -36,7 +42,19 @@ def process_map(x):
         test_graph.vertices, test_graph.optimized_graph)
     locations = resulting_map['locations']
     tag_verts = resulting_map['tags']
+    if visualize:
+        all_tags = graph_utils.get_tags_all_position_estimate(test_graph)
 
+        f = plt.figure()
+        ax = f.add_subplot(111, projection='3d')
+        plt.plot(locations[:, 0], locations[:, 1], locations[:, 2], '.', c='b', label='Odom Vertices')
+        plt.plot(original_tag_verts[:, 0], original_tag_verts[:, 1], original_tag_verts[:, 2], 'o', c='c', label='Tag Vertices')
+        plt.plot(tag_verts[:, 0], tag_verts[:, 1], tag_verts[:, 2], 'o', c='r', label='Tag Vertices')
+        for vert in tag_verts:
+            ax.text(vert[0], vert[1], vert[2], str(int(vert[-1])), color='black')
+        plt.plot(all_tags[:, 0], all_tags[:, 1], all_tags[:, 2], '.', c='g', label='All Tag Edges')
+        plt.legend()
+        plt.show()
     return tag_verts
 
 def make_processed_map_JSON(tag_locations):
@@ -50,6 +68,32 @@ def make_processed_map_JSON(tag_locations):
                        'odometry_vertices': [],
                        'waypoints_vertices': []})
 
+def process_map(map_name, map_json, visualize=False):
+    json_blob = bucket.get_blob(map_json)
+    if json_blob is not None:
+        json_data = json_blob.download_as_string()
+        x = json.loads(json_data)
+        tag_locations = optimize_map(x, False, visualize)
+        processed_map = make_processed_map_JSON(tag_locations)
+        processed_map_filename = os.path.basename(map_json)[:-5] + '_processed.json'
+        processed_map_full_path = os.path.join('TestProcessed', processed_map_filename)
+        processed_map_blob = bucket.blob(processed_map_full_path)
+        processed_map_blob.upload_from_string(processed_map)
+        db.reference('maps').child(map_name).child('map_file').set(processed_map_full_path)
+        print("processed map", map_name)
+    else:
+        print("map file was missing")
+
+def unprocessed_maps_callback(m):
+    if type(m.data) == str:
+        # a single new map just got added
+        process_map(m.path.lstrip('/'), m.data, True)
+    else:
+        # this is a dictionary of all the data that is there initially
+        for map_name, map_json in m.data.items():
+            pass
+            #process_map(map_name, map_json)
+
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
 
@@ -62,19 +106,5 @@ app = firebase_admin.initialize_app(cred, {
 ref = db.reference('/unprocessed_maps')
 to_process = ref.get()
 bucket = storage.bucket(app=app)
-# TODO: for some reason many of the maps have multiple copies of the same tag in the same frame
-for map_name, map_json in to_process.items():
-    print(map_name)
-    json_blob = bucket.get_blob(map_json)
-    if json_blob is not None:
-        json_data = json_blob.download_as_string()
-        x = json.loads(json_data)
-        tag_locations = process_map(x)
-        processed_map = make_processed_map_JSON(tag_locations)
-        processed_map_filename = os.path.basename(map_json)[:-5] + '_processed.json'
-        processed_map_full_path = os.path.join('TestProcessed', processed_map_filename)
-        processed_map_blob = bucket.blob(processed_map_full_path)
-        processed_map_blob.upload_from_string(processed_map)
-        db.reference('maps').child(map_name).child('map_file').set(processed_map_full_path)
-    else:
-        print("map file was missing")
+
+ref.listen(unprocessed_maps_callback)
