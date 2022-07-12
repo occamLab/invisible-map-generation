110aa67ef9567ae25665c008682af844a1b5e133
Rucha Dave <ruchadave16@gmail.com>
2022-07-12 09:44:20 -0400

    Added to documentation for sweep file

--- map_processing/sweep.py
+++ map_processing/sweep.py
@@ -78,12 +78,16 @@ def sweep_params(mi: MapInfo, ground_truth_data: dict, base_oconfig: OConfig,
     results: List[float] = []
     results_indices: List[int] = []
     results_first_oresult = results_tuples[0][2]
+
+    # Result_tuple: (gt metric: Float, index: Int, oresult: OResult)
     for result_tuple in results_tuples:
         results.append(result_tuple[0])
         results_indices.append(result_tuple[1])
 
     results_arr_dims = [len(sweep_arrs[key]) for key in ordered_sweep_config_keys]
     results_arr = np.ones(results_arr_dims) * -1
+
+    # Currently only uses result (metric) and result_idx
     for result, result_idx, result_oresult in results_tuples:
         result_arr_idx = []
         for key_idx, key in enumerate(ordered_sweep_config_keys):
@@ -92,20 +96,25 @@ def sweep_params(mi: MapInfo, ground_truth_data: dict, base_oconfig: OConfig,
     if np.any(results_arr < 0):
         raise Exception("Array of sweep results was not completely populated")
 
+    # Print results from sweep
     sweep_results = OSweepResults(
         gt_results_list=list(results_arr.flatten(order="C")), gt_results_arr_shape=list(results_arr.shape),
         sweep_config={item[0]: list(item[1]) for item in sweep_arrs.items()},
         sweep_config_keys_order=ordered_sweep_config_keys, base_oconfig=base_oconfig, map_name=mi.map_name,
         generated_params=UGDataSet.parse_obj(mi.map_dct).generated_from)
     if verbose:
-        print(f"Pre-optimization value: {results_first_oresult.gt_metric_pre:.3f}")
+        print(f"\n Pre-optimization value: {results_first_oresult.gt_metric_pre:.3f}")
         print(f"Minimum ground truth value: {sweep_results.min_gt_result:.3f} (delta is "
-              f"{results_first_oresult.gt_metric_pre - sweep_results.min_gt_result:.3f})")
+              f"{(sweep_results.min_gt_result - results_first_oresult.gt_metric_pre):.3f})")
+        print(f"")
         print("Parameters:\n" + json.dumps(sweep_results.args_producing_min, indent=2))
 
+    # Cache file from sweep
     results_cache_file_name_no_ext = f"{datetime.datetime.now().strftime(TIME_FORMAT)}_{mi.map_name}_sweep"
     if cache_results:
         CacheManagerSingleton.cache_sweep_results(sweep_results, results_cache_file_name_no_ext)
+
+    # Show heatmap from sweep
     if generate_plot:
         fig = sweep_results.visualize_results_heatmap()
         if show_plot:
@@ -117,8 +126,9 @@ def sweep_params(mi: MapInfo, ground_truth_data: dict, base_oconfig: OConfig,
 
 
 def _sweep_target(sweep_args_tuple: Tuple[Graph, OConfig, Dict[int, np.ndarray], Tuple[int, int], bool], last_run=False) \
-        -> Tuple[float, int]:
+        -> Tuple[float, int, OResult]:
     """Target callable used in the sweep_params function.
+    *****NOTE: This function is what individually optimizes each of the parameters provided through the sweep.
 
     Args:
         sweep_args_tuple: In order, contains: (1) The graph object to optimize (which is deep-copied before being passed
@@ -127,8 +137,12 @@ def _sweep_target(sweep_args_tuple: Tuple[Graph, OConfig, Dict[int, np.ndarray],
          pre-optimization metric being returned too.
 
     Returns:
-        Return value from GraphManager.optimize_graph
+        Return a tuple containing:
+            gt_result: Float representing ground truth value metric
+            sweep_args_tuple[3][0]: Int representing the index of the sweep parameter
+            oresult: OResult representing the result of GraphManager.optimize_graph
     """
+    # Same framework as holistic_optimize from graph_opt_hl_interface
     oresult = optimize_graph(graph=deepcopy(sweep_args_tuple[0]), oconfig=sweep_args_tuple[1], visualize=False)
     gt_result = ground_truth_metric_with_tag_id_intersection(
         optimized_tags=tag_pose_array_with_metadata_to_map(oresult.map_opt.tags),
@@ -136,6 +150,7 @@ def _sweep_target(sweep_args_tuple: Tuple[Graph, OConfig, Dict[int, np.ndarray],
     oresult.gt_metric_pre = ground_truth_metric_with_tag_id_intersection(
         optimized_tags=tag_pose_array_with_metadata_to_map(oresult.map_pre.tags),
         ground_truth_tags=sweep_args_tuple[2])
+
     if sweep_args_tuple[4]:
         print(f"Completed sweep (parameter idx={sweep_args_tuple[3][0] + 1})")
 
