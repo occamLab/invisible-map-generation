62a4a077b86c406466b0b615d6278d510d74279d
ayushchakra <achakraborty@olin.edu>
2022-11-08 04:06:40 -0500

    documented first part of experiment two

--- run_scripts/run.py
+++ run_scripts/run.py
@@ -1,26 +1,14 @@
 import os
-from re import search
 import sys
 repository_root = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), os.pardir)
 sys.path.append(repository_root)
-
-from map_processing import throw_out_bad_tags as tag_filter
 from map_processing.sweep import sweep_params
-from map_processing.graph_opt_utils import rotation_metric
-from map_processing.graph_opt_hl_interface import (
-    holistic_optimize,
-    WEIGHTS_DICT,
-    WeightSpecifier,
-)
-from map_processing.data_models import OComputeInfParams, GTDataSet, OConfig
+from map_processing.data_models import OComputeInfParams, OConfig
 from map_processing.cache_manager import CacheManagerSingleton
-from map_processing import PrescalingOptEnum, VertexType
-import map_processing
 from typing import Dict, Callable, Iterable, Any, Tuple
 from firebase_admin import credentials
 import numpy as np
-import argparse
 import pdb
 
 SBA_SWEEP_CONFIG: Dict[OConfig.OConfigEnum, Tuple[Callable, Iterable[Any]]] = {
@@ -38,6 +26,43 @@ NO_SBA_SWEEP_CONFIG: Dict[OConfig.OConfigEnum, Tuple[Callable, Iterable[Any]]] =
     # OConfig.OConfigEnum.TAG_SBA_VAR: (np.geomspace, [1e-10, 10, 10]),
     # OConfig.OConfigEnum.GRAV_MAG: (np.linspace, [1, 1, 1]),
 }
+
+def run_sweep(map_name, pso):
+    # Fetch the service account key JSON file contents
+    env_variable = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+    if env_variable is None:
+        cms = CacheManagerSingleton(firebase_creds=None, max_listen_wait=0)
+    else:
+        cms = CacheManagerSingleton(
+            firebase_creds=credentials.Certificate(env_variable), max_listen_wait=0
+        )
+
+    map_info = cms.find_maps(map_name).pop()
+
+    compute_inf_params = OComputeInfParams(
+        lin_vel_var=np.ones(3) * np.sqrt(3) * 1.0,
+        tag_sba_var=1.0,
+        ang_vel_var=1.0,
+    )
+
+    gt_dataset = cms.find_ground_truth_data_from_map_info(map_info)
+    sweep_config = NO_SBA_SWEEP_CONFIG if pso == 1 else SBA_SWEEP_CONFIG
+    sweep_one_result = sweep_params(
+        mi=map_info,
+        ground_truth_data=gt_dataset,
+        base_oconfig=OConfig(
+            is_sba= pso==0,
+            compute_inf_params=compute_inf_params,
+        ),
+        sweep_config=sweep_config,
+        ordered_sweep_config_keys=[key for key in sweep_config.keys()],
+        verbose=False,
+        generate_plot=True,
+        show_plot=False,
+        num_processes=12,
+        cms=cms,
+        simple_metrics=True
+    )
 
 def extrapolate_parameters(map_name_one, map_name_two, pso):
     # Fetch the service account key JSON file contents
@@ -73,7 +98,7 @@ def extrapolate_parameters(map_name_one, map_name_two, pso):
         verbose=False,
         generate_plot=True,
         show_plot=False,
-        num_processes=8,
+        num_processes=12,
         cms=cms,
         simple_metrics=True
     )
@@ -90,7 +115,7 @@ def extrapolate_parameters(map_name_one, map_name_two, pso):
         verbose=False,
         generate_plot=True,
         show_plot=False,
-        num_processes=8,
+        num_processes=12,
         cms=cms,
         simple_metrics=True
     )
